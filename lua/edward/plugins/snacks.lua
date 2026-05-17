de7f5815766de0eca7d835593f7cdac9a2840a36
edwardsharp2003 <edwardsharp2003@gmail.com>
2026-05-17 15:40:50 +0200

    ?

--- lua/edward/plugins/snacks.lua
+++ lua/edward/plugins/snacks.lua
@@ -156,7 +156,18 @@ return {
             { "<leader>pc", function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end, desc = "Find Config File" },
             { "<leader>ps", function() require("snacks").picker.grep() end, desc = "Grep word" },
             { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "Search Visual selection or Word", mode = { "n", "x" } },
-            { "<leader>pk", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, desc = "Search Keymaps (Snacks Picker)" },
+            {
+                "<leader>pk",
+                function()
+                    require("snacks").picker.keymaps({
+                        layout = {
+                            preset = "vertical",
+                            width = 0.4,
+                        }
+                    })
+                end,
+                desc = "Search Keymaps (Snacks Picker)"
+            },
 
             -- Git Stuff
             { "<leader>gbr", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc = "Pick and Switch Git Branches" },
